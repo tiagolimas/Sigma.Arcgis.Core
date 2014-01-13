283647baec9573871d4ecf661cf2e75ce58c4465
Tiago Lima <tiago@consultoriasigma.com.br>
2014-01-13 18:18:46 -0200

    primeira carga

---
+++ src/Sigma.Arcgis.Core.Testes/Geometria/CalculadoraTeste.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ESRI.ArcGIS.Geometry;
+using NUnit.Framework;
+using Sigma.Arcgis.Core.Geometria;
+
+namespace Sigma.Arcgis.Core.Testes.Geometria
+{
+    [TestFixture]
+    class CalculadoraTeste
+    {
+        [Test]
+        public void TestSoma1()
+        {
+            var c = new Calculadora();
+            int resultado = c.Soma(2, 3);
+
+            Assert.AreEqual(5, resultado);
+        }
+
+
+    }
+}
---
+++ src/Sigma.Arcgis.Core/Geometria/CalculoAzimuteDistancia.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.ArcMap;
+
+namespace Sigma.Arcgis.Core.Geometria
+{
+    public class CalculoAzimuteDistancia
+    {
+        public double CalculaDistanciaEntreDoisPontos(IPoint de, IPoint para)
+        {            
+            IPointCollection col = new PolylineClass();
+            col.AddPoint(de, Type.Missing, Type.Missing);
+            col.AddPoint(para, Type.Missing, Type.Missing);
+            IPolyline poly = (IPolyline)col;
+            
+            return poly.Length;
+        }
+
+        public double CalculaAzimute(IPoint de, IPoint para)
+        {
+            IVector3D vector = new Vector3DClass();
+            vector.ConstructDifference(para, de);
+            return CalculaGraus(vector.Azimuth);
+        }
+
+        public double CalculaGraus(double rad)
+        {
+            return rad * (180 / Math.PI);
+        }
+    }
+}
